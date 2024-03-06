aaccb886c63d86a7af35858d539d48f7e9fc28f5
HeYaoting666 <2077104031@qq.com>
2024-03-06 10:50:09 +0800

    Update

--- webserver/webserver.cpp
+++ webserver/webserver.cpp
@@ -101,10 +101,10 @@ void WebServer::init_sql_pool() {
 
     //在user表中检索username，passwd数据，浏览器端输入
     if(mysql_query(mysql, "SELECT username, passwd FROM user"))
-        LOG_ERROR("SELECT error:%s\n", mysql_error(mysql));
+        LOG_ERROR("SELECT error:%s\n", mysql_error(mysql))
 
     //从表中检索完整的结果集
-    MYSQL_RES *result = mysql_store_result(mysql);
+    MYSQL_RES* result = mysql_store_result(mysql);
 
     //从结果集中获取下一行，将对应的用户名和密码，存入map中
     while (MYSQL_ROW row = mysql_fetch_row(result)) {
